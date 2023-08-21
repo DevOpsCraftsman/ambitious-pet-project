af69a0f07789ae0c488dfd471672feda385a4aa8
Yahya Abou Imran <alexandre.poitevin.dev@gmail.com>
2023-08-21 09:48:40 +0100

    Progress in wc.cpp

--- cli/commands/implementations/cpp/wc.cpp
+++ cli/commands/implementations/cpp/wc.cpp
@@ -1,18 +1,44 @@
 #include <iostream>
 #include <fstream>
 
+using namespace std;
+
+void displayArgs(int argc, char *const *argv);
+
+
+void countLines(ifstream &file);
+
 int main(int argc, char *argv[]) {
-    // std::cout << "Hello, World!" << std::endl;
-    std::ifstream file(argv[1]);
+
+    displayArgs(argc, argv);
+
+    ifstream file(argv[1]);
+
     if (!file) {
-        std::cerr << "Error opening file." << std::endl;
+        cerr << "Error opening file." << endl;
         return 1;
     }
 
-    std::string line;
-    while (std::getline(file, line)) {
-        std::cout << line << std::endl;
+    countLines(file);
+
+    return 0;
+}
+
+void countLines(ifstream &file) {
+    string line;
+    int lineCount = 0;
+
+    while (getline(file, line)) {
+        lineCount++;
     }
     file.close();
-    return 0;
+
+    cout << lineCount << endl;
+}
+
+void displayArgs(int argc, char *const *argv) {
+    cout << argc << endl;
+    for (int i(1); i < argc; i++) {
+        cout << argv[i] << endl;
+    }
 }
