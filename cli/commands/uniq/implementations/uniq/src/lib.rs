ab246e8eb1a8c21e2206c296a82aa5446671335e
Yahya Abou Imran <alexandre.poitevin.dev@gmail.com>
2023-09-07 19:20:49 +0100

    Pass 4 tests

--- cli/commands/uniq/implementations/uniq/src/lib.rs
+++ cli/commands/uniq/implementations/uniq/src/lib.rs
@@ -1,5 +1,25 @@
 pub mod uniq {
-    pub fn uniq(lines: &str) -> &str {
-        return lines
+    use std::ops::Add;
+
+    pub fn uniq(content: &str) -> String {
+        let lines = content.lines();
+        if lines.count() <= 1 {
+            return String::from(content)
+        }
+        let mut output = String::from("");
+        let result = content.lines()
+            .zip(content.lines().skip(1))
+            .inspect(|(a, b)| println!("a: {}, b: {}", a, b))
+            .collect::<Vec<_>>();
+        let mut first_loop = true;
+        for (line1, line2) in result {
+            if first_loop {
+                output = output.add(line1)
+            }
+            if line1 != line2 {
+                output = output.add("\n").add(line2);
+            }
+        }
+        return output;
     }
 }
--- cli/commands/uniq/implementations/uniq/tests/test_uniq.rs
+++ cli/commands/uniq/implementations/uniq/tests/test_uniq.rs
@@ -1,15 +1,35 @@
 #[cfg(test)]
-
 mod test {
     use uniq::uniq::uniq;
 
-    #[test]
-    fn test_uniq() {
-        let s0 = "";
-        assert_eq!(uniq(s0), "");
-        let s1 = "line";
-        assert_eq!(uniq(s1), "line");
-        let s2 = "line\nother";
-        assert_eq!(uniq(s2), "line\nother");
+    macro_rules! uniq_test {
+    ($($name:ident: $value:expr,)*) => {
+        $(
+            #[test]
+            fn $name() {
+                let (input, expected) = $value;
+                assert_eq!(expected, uniq(input));
+            }
+        )*
+        }
+    }
+
+    uniq_test! {
+        empty: (
+            "",
+            ""
+        ),
+        one_line: (
+            "line",
+            "line",
+        ),
+        two_different_line: (
+            "line1\nline2",
+            "line1\nline2",
+        ),
+        two_similar_lines: (
+            "line1\nline1",
+            "line1",
+        ),
     }
 }
