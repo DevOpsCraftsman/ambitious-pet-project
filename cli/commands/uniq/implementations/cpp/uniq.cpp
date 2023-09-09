6a8c248740ed8da15e3a9a6b97497f5ca65b68a2
Yahya Abou Imran <alexandre.poitevin.dev@gmail.com>
2023-09-09 11:38:17 +0100

    WIP uniq -l

--- cli/commands/uniq/implementations/cpp/uniq.cpp
+++ cli/commands/uniq/implementations/cpp/uniq.cpp
@@ -5,58 +5,98 @@ using namespace std;
 
 bool match(string actualLine, string prevLine, string option);
 
-int main(int argc, char *argv[]) {
-
-    ifstream file(argv[1]);
-    string option;
-    if (argc == 3) {
-        option = argv[2];
-    }
-
-    if (!file) {
-        cerr << "Error opening file." << endl;
-        return 1;
-    }
-
-    if (option == "-u") {
-
-        string actualLine;
-        string nextLine;
-        bool noDuplicate = true;
-
-        while (getline(file, nextLine)) {
-            if (actualLine == nextLine) {
-                noDuplicate = false;
-            } else if (noDuplicate) {
-                cout << actualLine << endl;
-            } else {
-                noDuplicate = true;
+string uniq_u(ifstream &file) {
+    string currentLine;
+    string previousLine;
+    bool firstLine = true;
+    bool previousLineIsDuplicate = false;
+    string result;
+    while (getline(file, currentLine)) {
+        bool currentLineIsDuplicate = (currentLine == previousLine);
+        if (!previousLineIsDuplicate) {
+            if (!currentLineIsDuplicate) {
+                if (!firstLine) {
+                    result.append("\n");
+                }
+                result.append(currentLine);
+                firstLine = false;
             }
-            actualLine = nextLine;
         }
-        if (nextLine != actualLine) {
-            cout << actualLine << endl;
+        previousLineIsDuplicate = currentLineIsDuplicate;
+        previousLine = currentLine;
+
+        firstLine = false;
+    }
+    return result;
+}
+
+void test_uniq_l() {
+
+    const int row = 3;
+    const int col = 2;
+    string test_data[row][col] = {
+            {"file1.txt", "line1"},
+            {"file2.txt", "line1\nline2"},
+            {"file3.txt", "line1"},
+    };
+
+    for (int i = 0; i < row; i++) {
+        string path = "./test_data/" + test_data[i][0];
+        string expected = test_data[i][1];
+        ifstream file(path);
+        string result = uniq_u(file);
+        cout << "testing: " + path << endl;
+        if (result == expected) {
+            cout << "passed." << endl;
+        } else {
+            cerr << "failed: " + result << endl;
+            cerr << "expected: " + expected << endl;
         }
-
-    } else {
-
-        string actualLine;
-        string previousLine;
-
-        while (getline(file, actualLine)) {
-            if (match(actualLine, previousLine, option)) {
-                cout << actualLine << endl;
-            }
-            previousLine = actualLine;
-        }
-
     }
 
+}
 
-    file.close();
-
+int main() {
+    test_uniq_l();
+    // Add more test functions here as needed.
     return 0;
 }
+//int main(int argc, char *argv[]) {
+//
+//    ifstream file(argv[1]);
+//    string option;
+//    if (argc == 3) {
+//        option = argv[2];
+//    }
+//
+//    if (!file) {
+//        cerr << "Error opening file." << endl;
+//        return 1;
+//    }
+//
+//    if (option == "-u") {
+//
+//        uniq_u(file);
+//
+//    } else {
+//
+//        string actualLine;
+//        string previousLine;
+//
+//        while (getline(file, actualLine)) {
+//            if (match(actualLine, previousLine, option)) {
+//                cout << actualLine << endl;
+//            }
+//            previousLine = actualLine;
+//        }
+//
+//    }
+//
+//
+//    file.close();
+//
+//    return 0;
+//}
 
 bool match(string actualLine, string prevLine, string option) {
     if (option == "-d" && actualLine == prevLine) {
